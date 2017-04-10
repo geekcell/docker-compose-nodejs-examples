ef3c2ddd2370ec3514b97e42e42e7390db14bff9
Pascal Cremer <b00gizm@gmail.com>
2017-04-10 13:26:28 +0200

    Add Comment form for Express template

--- templates/express/site/router.js
+++ templates/express/site/router.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const database = require('../database');
 const router = new express.Router();
 
-const home = (req, res) => res.render('index');
+const saveComment = (req, res) => {
+  const { author, body } = req.body;
+
+  if (!author || !body) {
+    return res.redirect('/');
+  }
+
+  return database
+    .addNewPost(author, body)
+    .then(
+      () => res.redirect('/'),
+      err => res.status(500).send('Oops, something went wrong!')
+    )
+  ;
+}
+
+const home = (req, res) => {
+  return database
+    .getPosts()
+    .then(
+      posts => res.render('index', { posts }),
+      err => res.status(500).send('Oops, something went wrong!')
+    )
+  ;
+};
 
 router.get('/', home);
+router.post('/', saveComment);
 
 module.exports = router;
