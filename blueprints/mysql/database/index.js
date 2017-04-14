737f6861da8686911b3272798be125f0788fc69a
Pascal Cremer <b00gizm@gmail.com>
2017-04-14 21:07:46 +0200

    Fix compatibility for database module

--- blueprints/mysql/database/index.js
+++ blueprints/mysql/database/index.js
@@ -11,75 +11,79 @@ let connection;
 const init = ({ host, port, user, password, database } = {
   host: dbHost,
   port: dbPort,
-	user: dbUser,
-	password: dbPass,
+  user: dbUser,
+  password: dbPass,
   database: dbName
 }) => {
-	connection = mysql.createConnection({
-		host,
-		port,
-		user,
-		password,
-		database
-	});
+  connection = mysql.createConnection({
+    host,
+    port,
+    user,
+    password,
+    database
+  });
 
   // For a real world app, you probably want to check if there were any
   // any exceptions and handle them...
-	createTable();
+  createTable();
 };
 
 const createTableStmt = `
 CREATE TABLE IF NOT EXISTS comments (
-	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	author VARCHAR(100) NOT NULL,
-	body TEXT NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  author VARCHAR(100) NOT NULL,
+  body TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 )
 `;
 
 const createTable = () => new Promise((resolve, reject) => {
-	connection.query(createTableStmt, err => {
-		if (err) {
-			return reject(err);
-		}
+  connection.query(createTableStmt, err => {
+  if (err) {
+    return reject(err);
+  }
 
-		return resolve();
-	});
+  return resolve();
+  });
 });
 
 const addNewPost = (author, body) => new Promise((resolve, reject) => {
-	connection.query('INSERT INTO comments SET ?', { author, body }, err => {
-		if (err) {
-			return reject(err);
-		}
+  connection.query(
+    'INSERT INTO comments SET ?',
+    { author, body },
+    (err, result) => {
+      if (err) {
+        return reject(err);
+      }
 
-		return resolve();
-	});
+      return resolve(result.insertId);
+    }
+  );
 });
 
 const getPosts = () => new Promise((resolve, reject) => {
-	connection.query('SELECT * FROM comments', (err, results) => {
-		if (err) {
-			return reject(err);
-		}
+  connection.query('SELECT * FROM comments', (err, results) => {
+    if (err) {
+      return reject(err);
+    }
 
-		return resolve(results);
-	});
+    return resolve(results);
+  });
 });
 
 const getPostById = id => new Promise((resolve, reject) => {
-	connection.query(
-		'SELECT * FROM comments WHERE id = ? LIMIT 1',
-		[id],
-		(err, results) => {
-			if (err) {
-				return reject(err);
-			}
+  connection.query(
+    'SELECT * FROM comments WHERE id = ? LIMIT 1',
+    [id],
+    (err, results) => {
+      if (err) {
+        return reject(err);
+      }
 
-			return resolve(results);
-		}
-	);
+      return resolve(results);
+    }
+  );
 });
 
 module.exports = { init, addNewPost, getPosts, getPostById }
