e5bbff37f2c0907f83064f7888a4efe3cb98bf24
janvt <janvt@me.com>
2023-03-03 13:25:57 +0100

    modernize nodemon + redis example

--- 02-express-redis-nodemon/app/app.js
+++ 02-express-redis-nodemon/app/app.js
@@ -1,60 +1,27 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const {createClient} = require("redis");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const app = express();
+const port = 3000;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+const client = createClient({
+    url: 'redis://db:6379'
 });
 
-// error handlers
+client.on('error', err => console.log('Redis Client Error', err));
+(async() => { await client.connect(); })();
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+app.get('/', async (req, res) => {
+    let body = '';
+    await client.set('foo', 'bar');
+    body += 'Write value "bar" to key "foo"... ';
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    const bar = await client.get('foo');
+    body += `Read key "foo", got value "${bar}".`;
+
+    res.send(body);
 });
 
-
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
